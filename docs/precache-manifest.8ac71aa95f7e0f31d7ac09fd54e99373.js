29b8cc37685172b65812448e8f430a3c338bcb78
Alver Grisales <viga.23@hotmail.com>
2020-07-07 08:50:50 -0500

    feat: rename folder build to docs

---
+++ docs/precache-manifest.8ac71aa95f7e0f31d7ac09fd54e99373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0787c17f5f1a858b4971718052864151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b3ea7c48478e7e6acc",
+    "url": "/static/css/main.598186a2.chunk.css"
+  },
+  {
+    "revision": "5b99211f9330a2fc0b38",
+    "url": "/static/js/2.b0045bbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0045bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b3ea7c48478e7e6acc",
+    "url": "/static/js/main.b5b24358.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
